e2eaf020bb46cac51cdc85b2e6f490a22207ca8e
18652082378 <1457662219@qq.com>
2026-07-19 02:40:01 +0800

    release: build 17 app icon

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.TURTLE_API_BASE_URL = "https://api.turtleworld.cn";
 // iOS 的构建号。每次提交新构建时与 Xcode 的 CURRENT_PROJECT_VERSION 保持一致。
-window.TURTLE_APP_BUILD = 16;
+window.TURTLE_APP_BUILD = 17;
 window.TURTLE_APP_STORE_URL = "https://apps.apple.com/app/id6783481335";
