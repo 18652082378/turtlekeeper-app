262080953a4c8d87d103fbfc8891729215602c49
18652082378 <1457662219@qq.com>
2026-08-09 06:27:55 +0800

    release: build 52

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ window.TURTLE_API_BASE_URL = "https://api.turtleworld.cn";
 // market 参数的链接识别为 Universal Link，并在已安装 App 时直接打开商品详情。
 window.TURTLE_PUBLIC_APP_URL = "https://api.turtleworld.cn/";
 // iOS 的构建号。每次提交新构建时与 Xcode 的 CURRENT_PROJECT_VERSION 保持一致。
-window.TURTLE_APP_BUILD = 51;
+window.TURTLE_APP_BUILD = 52;
 window.TURTLE_APP_STORE_URL = "https://apps.apple.com/app/id6783481335";
